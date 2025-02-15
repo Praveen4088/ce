888f60c48e8a9e2a46f28ea51fd39773b2d4b02b
Praveen4088 <praveencse2023@gmail.com>
2025-02-15 09:10:45 +0530

    done

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -6,15 +6,16 @@ const Homepage = () => {
       <div className="row justify-content-center align-items-center min-vh-100">
         <div className="col-12 col-md-6">
           <div className="card shadow-sm p-2 mb-5 bg-secondry rounded">
-            <div className="card-body text-center bg-dark">
-              <img className="img-fluid mx-auto d-block" src="/assets/react.svg"/>
+            {/* <div className="card-body text-center bg-dark"> */}
+              {/* <img className="img-fluid mx-auto d-block" src="/assets/react.svg"/> */}
+              <p color='white'>This is home page</p>
 
             </div>
           </div>
         </div>
       </div>
       
-    </div>
+    // </div>
   )
 }
 
